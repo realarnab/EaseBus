b0c90d96e590f6e2f0b2df5e4b4d91dc89e09d17
Arnab <arnabmondal604@gmail.com>
2024-07-19 21:52:16 +0530

    init

---
+++ src/main/java/com/EaseBus/EaseBusApplication.java
@@ -0,0 +1,13 @@
+package com.EaseBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaseBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaseBusApplication.class, args);
+	}
+
+}
