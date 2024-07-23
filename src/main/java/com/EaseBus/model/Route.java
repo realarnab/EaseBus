8223fbbb7d5214fc86616ed57ea28ebb89b82d3a
Arnab <arnabmondal604@gmail.com>
2024-07-23 21:03:15 +0530

    route entity added

--- src/main/java/com/EaseBus/model/Route.java
+++ src/main/java/com/EaseBus/model/Route.java
@@ -1,2 +1,20 @@
-package com.EaseBus.model;public class Route {
+package com.EaseBus.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Entity
+@Data
+public class Route {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer routeId;
+    @Column(name = "route_from")
+    private String routeFrom;
+    @Column(name = "route_to")
+    private String routeTo;
+    @NotNull(message = "distance can not be null")
+    private double distance;
+
 }
