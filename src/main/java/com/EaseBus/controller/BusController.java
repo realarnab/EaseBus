1dcb892ebb4fca3127d34529f998ed38b311008c
Arnab <arnabmondal604@gmail.com>
2024-07-23 21:04:40 +0530

    add bus api added

--- src/main/java/com/EaseBus/controller/BusController.java
+++ src/main/java/com/EaseBus/controller/BusController.java
@@ -1,2 +1,25 @@
-package com.EaseBus.controller;public class BusController {
+package com.EaseBus.controller;
+
+import com.EaseBus.model.Bus;
+import com.EaseBus.service.BusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/buses")
+public class BusController {
+
+    @Autowired
+    private BusService busService;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addBus(@RequestBody Bus bus){
+        Bus newBus = busService.addNewBus(bus);
+        return new ResponseEntity<>(newBus, HttpStatus.CREATED);
+    }
 }
