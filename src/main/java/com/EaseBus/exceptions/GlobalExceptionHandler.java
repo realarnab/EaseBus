cb13ac51874f691c52308d368726243563e6c1cd
Arnab <arnabmondal604@gmail.com>
2024-08-05 20:17:13 +0530

    busNotFound exception added

--- src/main/java/com/EaseBus/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/EaseBus/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,18 @@
 package com.EaseBus.exceptions;
 
+import jakarta.servlet.annotation.WebFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(BusNotFoundException.class)
+    public ResponseEntity<?> busNotFoundExceptionHandler(BusNotFoundException e, WebRequest webRequest){
+        return new ResponseEntity<>(e.getMessage()+webRequest.getDescription(true), HttpStatus.NOT_FOUND);
+    }
 
 }
